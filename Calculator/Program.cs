751c3370d72017a4265799864e551b94ec9dc350
ogaslin <olivia.gaslin@gmail.com>
2021-05-10 14:00:41 -0400

    Fixed operational errors

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,70 +1,87 @@
 ﻿using System;
 
+
 namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please type number of operation you wish to perform.");
-            Console.WriteLine("1: add");
-            Console.WriteLine("2: subtract");
-            Console.WriteLine("3: multiply");
-            Console.WriteLine("4: divide");
-            Console.WriteLine("5: modulo");
-            Console.WriteLine("6: raise by power(base and then exponent)");
-            Console.WriteLine("7: factorial");
-            string inputText = Console.ReadLine();
-            int selection;
-            int.TryParse(inputText, out selection);
-            Console.WriteLine("Please type the first operand and press enter.");
-            string input1 = Console.ReadLine();
-            Console.WriteLine("Please type the second operand and press enter.");
-            string input2 = Console.ReadLine();
+            bool run = true;
 
-            int x = Int32.Parse(input1);
-            int y = Int32.Parse(input2);
-
-             switch(selection)
+            while (run)
             {
-
-                case 1:
-                    Console.WriteLine("Result: " + (x + y));
-                    break;
-                case 2:
-                    Console.WriteLine("Result: " + (x - y));
-                    break;
-
-                case 3:
-                    Console.WriteLine("Result: " + (x * y));
-                    break;
-
-                case 4:
-                    Console.WriteLine("Result: " + (x / y));
-                    break;
-
-                case 5:
-                    Console.WriteLine("Result: " + " x % y");
-                    break;
-
-                case 6:
-                    Console.WriteLine("Result: " + Math.Pow(x, y));
-                    break;
-
-                case 7:
-
-                    Console.WriteLine("Results: " +
-                        factorial(x));
-                    break;
+                Console.WriteLine("Please type number of operation you wish to perform and press enter.");
+                Console.WriteLine("1: add");
+                Console.WriteLine("2: subtract");
+                Console.WriteLine("3: multiply");
+                Console.WriteLine("4: divide");
+                Console.WriteLine("5: modulo");
+                Console.WriteLine("6: raise by power(base and then exponent)");
+                Console.WriteLine("7: factorial");
+                string inputText = Console.ReadLine();
+                int selection;
+                int.TryParse(inputText, out selection);
+                Console.WriteLine("Please type the first operand and press enter.");
+                string input1 = Console.ReadLine();
+                //if selection isn't factorial.
+                int y = 0;
+                if (selection != 7)
+                {
+                    Console.WriteLine("Please type the second operand and press enter.");
+                    string input2 = Console.ReadLine();
+                    y = Int32.Parse(input2);
+                }
+                int x = Int32.Parse(input1);
 
 
-                default:
+                switch (selection)
+                {
 
-                    break;
+                    case 1:
+                        Console.WriteLine("Result: " + (x + y));
+                        break;
+                    case 2:
+                        Console.WriteLine("Result: " + (x - y));
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Result: " + (x * y));
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Result: " + (x / y));
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Result: " +  (x % y));
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Result: " + Math.Pow(x, y));
+                        break;
+
+                    case 7:
+
+                        Console.WriteLine("Results: " +
+                            factorial(x));
+                        break;
+
+
+                    default:
+                        Console.WriteLine("The number does not match an operation.");
+                        break;
+
+
+                }
+                Console.WriteLine("Press Y key if you wish to perform another operation.");
+                string input3 = Console.ReadLine();
+                if (input3 != "Y")
+                {
+                    run = false;
+                }
                 
-       
             }
-
             //write lines to describe options to user and give each operation a number;
             //read the input from the user.  Should be number associated with the operations.
             //prompt user to enter first number.
